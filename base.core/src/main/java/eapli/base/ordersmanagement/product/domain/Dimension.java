af9ad3dc3b7d87442a6e68c97a9a3b479f8a1f8b
Marta Portugal <1201845@isep.ipp.pt>
2022-04-25 00:25:34 +0100

    product change folder

---
+++ base.core/src/main/java/eapli/base/ordersmanagement/product/domain/Dimension.java
@@ -0,0 +1,30 @@
+package eapli.base.ordersmanagement.product.domain;
+
+import eapli.framework.domain.model.ValueObject;
+
+import javax.persistence.Embeddable;
+
+/**
+ *
+ * @author João Tiago Araújo <<1200584@isep.ipp.pt>>
+ */
+
+@Embeddable
+public class Dimension implements ValueObject {
+
+    private Long width;
+
+    private Long height;
+
+    private Long lenght;
+
+    public Dimension(Long width, Long height, Long lenght) {
+        this.width = width;
+        this.height = height;
+        this.lenght = lenght;
+    }
+
+    public Dimension() {
+
+    }
+}
