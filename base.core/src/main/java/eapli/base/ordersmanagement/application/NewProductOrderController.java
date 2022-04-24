6755b28c12046798bde1cfe6835f3c840ce2a635
Marta Portugal <1201845@isep.ipp.pt>
2022-04-25 00:16:14 +0100

    change of local folder of files

--- base.core/src/main/java/eapli/base/ordersmanagement/application/NewProductOrderController.java
+++
@@ -1,9 +0,0 @@
-package eapli.base.ordersmanagement.application;
-
-import eapli.base.infrastructure.persistence.PersistenceContext;
-import eapli.base.ordersmanagement.repositories.OrderRepository;
-
-public class NewProductOrderController {
-
-    private final OrderRepository catalogueRepository = PersistenceContext.repositories().catalogues();
-}
--- base.core/src/main/java/eapli/base/ordersmanagement/domain/PriceOrder.java
+++
@@ -1,9 +0,0 @@
-package eapli.base.ordersmanagement.domain;
-
-import eapli.framework.domain.model.ValueObject;
-
-import javax.persistence.Embeddable;
-
-@Embeddable
-public class PriceOrder implements ValueObject {
-}
