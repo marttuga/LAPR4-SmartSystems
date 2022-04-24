e03d80c17b27ceefe2f2289e5fd6272e2fc0102b
Marta Portugal <1201845@isep.ipp.pt>
2022-04-25 00:26:20 +0100

    change folder and alterations on classes order and product

---
+++ base.core/src/main/java/eapli/base/ordersmanagement/order/domain/LineOrder.java
@@ -0,0 +1,9 @@
+package eapli.base.ordersmanagement.order.domain;
+
+import eapli.framework.domain.model.ValueObject;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class LineOrder implements ValueObject {
+}
---
+++ base.core/src/main/java/eapli/base/ordersmanagement/order/domain/PriceOrder.java
@@ -0,0 +1,9 @@
+package eapli.base.ordersmanagement.order.domain;
+
+import eapli.framework.domain.model.ValueObject;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class PriceOrder implements ValueObject {
+}
