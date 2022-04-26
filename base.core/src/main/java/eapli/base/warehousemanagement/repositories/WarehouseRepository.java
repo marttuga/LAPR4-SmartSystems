5b8676be5477668411a90436c45261e46dc47899
Maria Flores <1201615@isep.ipp.pt>
2022-04-26 14:49:14 +0100

    <L2L20-5> US2001 Implementation: classes of repositories

---
+++ base.core/src/main/java/eapli/base/warehousemanagement/repositories/WarehouseRepository.java
@@ -0,0 +1,22 @@
+package eapli.base.warehousemanagement.repositories;
+
+import eapli.base.warehousemanagement.domain.Warehouse;
+import eapli.base.warehousemanagement.domain.WarehouseID;
+import eapli.framework.domain.repositories.DomainRepository;
+
+import java.util.Optional;
+
+public interface WarehouseRepository extends DomainRepository<WarehouseID,Warehouse> {
+    /**
+     * returns the order with the given id
+     *
+     * @param warehouseID
+     * @return
+     */
+    default Optional<Warehouse> findByID(WarehouseID warehouseID) {
+        return ofIdentity(warehouseID);
+    }
+
+
+
+}
