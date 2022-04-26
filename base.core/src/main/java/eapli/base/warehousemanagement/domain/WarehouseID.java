6da594cfc29a2bd0595918f4f9a7ffc7b5f6b74b
Maria Flores <1201615@isep.ipp.pt>
2022-04-26 14:45:45 +0100

    <L2L20-5> US2001 Implementation: classes of domain

---
+++ base.core/src/main/java/eapli/base/warehousemanagement/domain/WarehouseID.java
@@ -0,0 +1,40 @@
+package eapli.base.warehousemanagement.domain;
+
+import eapli.framework.domain.model.ValueObject;
+import eapli.framework.strings.util.StringPredicates;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+
+@Embeddable
+public class WarehouseID implements ValueObject, Comparable<WarehouseID>{
+
+
+    private String warehouseID;
+
+    public WarehouseID(final String warehouseIdentification) {
+        if (StringPredicates.isNullOrEmpty(warehouseIdentification)) {
+            throw new IllegalArgumentException(
+                    "WarehouseID should neither be null nor empty");
+        }
+        this.warehouseID = warehouseIdentification;
+    }
+
+    protected WarehouseID() {
+    }
+
+    public static WarehouseID valueOf(final String warehouseIdentification) {
+        return new WarehouseID(warehouseIdentification);
+    }
+
+
+    @Override
+    public int compareTo(WarehouseID o) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "WarehouseID: " + warehouseID;
+    }
+}
