89128837468b733cf12287343265fa50f62694bd
João Tiago Araújo <1200584@isep.ipp.pt>
2022-04-26 11:51:13 +0100

    <L2L20-1> US1001: Implementation: Updated classes Product, Barcode, Dimension, ShortDescription

--- base.core/src/main/java/eapli/base/ordersmanagement/product/domain/ShortDescription.java
+++ base.core/src/main/java/eapli/base/ordersmanagement/product/domain/ShortDescription.java
@@ -1,8 +1,11 @@
 package eapli.base.ordersmanagement.product.domain;
 
 import eapli.framework.domain.model.ValueObject;
+import eapli.framework.strings.util.StringPredicates;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -10,16 +13,39 @@ import javax.persistence.Embeddable;
  */
 
 @Embeddable
-public class ShortDescription implements ValueObject {
+public class ShortDescription implements ValueObject, Comparable<ShortDescription> {
 
-
+    @Column(name = "shortDescription")
     private String description;
 
-    public ShortDescription(String description) {
+    public ShortDescription(String description) throws IllegalAccessException {
+        if(StringPredicates.isNullOrWhiteSpace(description)) {
+            throw new IllegalAccessException("The short description cannot be null or empty!");
+        }
+        if(description.length() > 30){
+            throw new IllegalArgumentException("The description should not have more than 30 characters.");
+        }
+        Pattern descRegerx = Pattern.compile("");
         this.description = description;
     }
 
-    public ShortDescription() {
 
+    protected ShortDescription() {
+
+    }
+
+    @Override
+    public String toString(){
+        return description;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.description.hashCode();
+    }
+
+    @Override
+    public int compareTo(ShortDescription o) {
+        return 0;
     }
 }
