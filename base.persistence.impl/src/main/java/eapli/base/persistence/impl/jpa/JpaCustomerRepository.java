90724c1fdd52cd06afd921f745e7c41809efa7e4
Rui Dias <1200963@isep.ipp.pt>
2022-04-28 20:54:07 +0100

    <L2L20-3> Customer controller changes

--- base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaCustomerRepository.java
+++ base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaCustomerRepository.java
@@ -1,17 +1,18 @@
 package eapli.base.persistence.impl.jpa;
 
 import eapli.base.ordersmanagement.customer.domain.Customer;
-import eapli.base.ordersmanagement.customer.domain.CustomerId;
-import eapli.base.ordersmanagement.customer.domain.EmailAddress;
+import eapli.base.ordersmanagement.customer.domain.MecanographicNumber;
+
 import eapli.base.ordersmanagement.customer.repositories.CustomerRepository;
 import eapli.framework.domain.repositories.TransactionalContext;
+import eapli.framework.general.domain.model.EmailAddress;
 import eapli.framework.infrastructure.authz.domain.model.Username;
 import eapli.framework.infrastructure.repositories.impl.jpa.JpaAutoTxRepository;
 
 import javax.persistence.TypedQuery;
 import java.util.*;
 
-public class JpaCustomerRepository extends JpaAutoTxRepository<Customer, CustomerId, CustomerId> implements CustomerRepository {
+public class JpaCustomerRepository extends JpaAutoTxRepository<Customer, MecanographicNumber, MecanographicNumber> implements CustomerRepository {
 
     public JpaCustomerRepository(TransactionalContext autoTx) {
         super(autoTx, "customerId");
@@ -19,19 +20,26 @@ public class JpaCustomerRepository extends JpaAutoTxRepository<Customer, Custome
 
 
     @Override
-    public Customer findByCustomerId(CustomerId customerId) {
+    public Optional<Customer> findByUsername(Username username) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Customer findByCustomerId(MecanographicNumber mecanographicNumber) {
         TypedQuery<Customer> query = super.createQuery("SELECT c FROM Customer c WHERE c.customerId = :customerId", Customer.class);
-        query.setParameter("customerId", customerId);
+        query.setParameter("customerId", mecanographicNumber);
         return query.getSingleResult();
     }
 
-
     @Override
-    public Optional<Customer> findByEmail(EmailAddress emailAddress) {
+    public Optional<Customer> findByEmail(EmailAddress email) {
         final Map<String, Object> param = new HashMap<>();
-        param.put("emailAddress", emailAddress);
+        param.put("emailAddress", email);
         return matchOne("e.systemUser.email =:emailAddress", param);
     }
+
+
+
 
     @Override
     public List<Customer> findAllCustomers() {
@@ -43,12 +51,12 @@ public class JpaCustomerRepository extends JpaAutoTxRepository<Customer, Custome
         return customerList;
     }
 
-    @Override
+  /*  @Override
     public Optional<Customer> findByUsername(Username name) {
         final Map<String, Object> params = new HashMap<>();
         params.put("name", name);
         return matchOne("e.systemUser.username =:name", params);
-    }
+    }*/
 
 
 
