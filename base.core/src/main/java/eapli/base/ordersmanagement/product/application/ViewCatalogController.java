283fd83be8331990f95d75e80b879710e449a835
Marta Portugal <1201845@isep.ipp.pt>
2022-04-28 02:51:31 +0100

    order and main menu alteration and addition of UI

--- base.core/src/main/java/eapli/base/ordersmanagement/product/application/ViewCatalogController.java
+++ base.core/src/main/java/eapli/base/ordersmanagement/product/application/ViewCatalogController.java
@@ -7,24 +7,43 @@ import eapli.base.ordersmanagement.product.domain.Product;
 import eapli.base.ordersmanagement.product.domain.ShortDescription;
 import eapli.base.ordersmanagement.product.repositories.ProductRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class ViewCatalogController {
     private final ProductRepository productRepository = PersistenceContext.repositories().products();
 
-    public Iterable<Product> getAllProducts() {
-        return productRepository.findAll();
-    }
-
-    public Optional<Product> getProductByBrand(Brand brand) {
+    public List<Product> getProductByBrand(Brand brand) {
         return productRepository.findByBrand(brand);
     }
-    public Optional<Product> getProductByCategory(Category category) {
-        return productRepository.findByCategory(category);
-    }
-    public Optional<Product> getProductByDescription(ShortDescription shortDescription) {
-        return productRepository.findByDescription(shortDescription);
-    }
+    public List<Product> getProductByCategory(Category category) {return productRepository.findByCategory(category);}
+    public List<Product> getProductByDescription(ShortDescription shortDescription) { return productRepository.findByDescription(shortDescription);}
 
-
+    public List<Product> getProductByBrandCategory(Brand brand,Category category) {
+        return productRepository.findByBrandCategory(brand,category);
+    }
+    public List<Product> getProductByCategoryDescription(Category category,ShortDescription shortDescription) {
+        return productRepository.findByDescriptionCategory(shortDescription,category);
+    }
+    public List<Product> getProductByBrandDescription(Brand brand,ShortDescription shortDescription) {
+        return productRepository.findByBrandDescription(brand,shortDescription);
+    }
+    public List<Product> getProductByBrandDescriptionCategory(Brand brand,ShortDescription shortDescription, Category category) {
+        return productRepository.findByBrandDescriptionCategory(brand,shortDescription,category);
+    }
+    public List<Product> findAllProducts(){
+        return productRepository.findAllProducts();
+    }
+    public List<Brand> findAllBrands(){
+        return productRepository.findAllBrands();
+    }
+    public Product findByProductCode(String code){
+        return productRepository.findByProductCode(code);
+    }
+    public Brand findByBrandName(String brandName){
+        return productRepository.findByBrandName(brandName);
+    }
+    public ShortDescription findByShortDescription(String shortDescription){
+        return productRepository.findByShortDescription(shortDescription);
+    }
 }
