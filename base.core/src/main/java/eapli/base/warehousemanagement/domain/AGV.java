41d1e99eeb0fdb843a65e66409756754ad6a3f29
João Tiago Araújo <1200584@isep.ipp.pt>
2022-04-29 14:09:12 +0100

    <L2L20-8> US2002: Implementation: Updated AGV, AGVDock, MaxWeightCarry, Model and Task

--- base.core/src/main/java/eapli/base/warehousemanagement/domain/AGV.java
+++ base.core/src/main/java/eapli/base/warehousemanagement/domain/AGV.java
@@ -16,6 +16,8 @@ public class AGV implements AggregateRoot<Identifier> {
 
     private int autonomy;
 
+    private AGVDock agvDock;
+
     private String AGVDescription;
 
     private Model model;
@@ -24,18 +26,26 @@ public class AGV implements AggregateRoot<Identifier> {
 
     private Task task;
 
-
     protected AGV(){
 
     }
 
-    public AGV(Identifier identifier, int autonomy, String AGVDescription, Model model, MaxWeightCarry maxWeightCarry, Task task) {
+    public AGV(Identifier identifier, int autonomy, AGVDock agvDock, String AGVDescription, Model model, MaxWeightCarry maxWeightCarry, Task task) {
         this.identifier = identifier;
         this.autonomy = autonomy;
+        this.agvDock = agvDock;
         this.AGVDescription = AGVDescription;
         this.model = model;
         this.maxWeightCarry = maxWeightCarry;
         this.task = task;
+    }
+
+    public static int valueOfAutonomy(final int autonomy) {
+        return (autonomy);
+    }
+
+    public static String valueOfDescription(final String description) {
+        return description;
     }
 
     @Override
