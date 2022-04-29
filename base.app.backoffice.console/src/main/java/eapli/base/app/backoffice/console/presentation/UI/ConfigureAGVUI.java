85913c25edf79f9b7c69b16af433276a68dd1684
João Tiago Araújo <1200584@isep.ipp.pt>
2022-04-29 14:10:17 +0100

    <L2L20-8> US2002: Implementation: Added ConfigureAGVController, ConfigureAGVUI

---
+++ base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/UI/ConfigureAGVUI.java
@@ -0,0 +1,40 @@
+package eapli.base.app.backoffice.console.presentation.UI;
+
+import eapli.base.app.backoffice.console.presentation.MainMenu;
+import eapli.base.warehousemanagement.application.ConfigureAGVController;
+import eapli.base.warehousemanagement.domain.*;
+import eapli.framework.domain.repositories.IntegrityViolationException;
+import eapli.framework.io.util.Console;
+
+public class ConfigureAGVUI {
+
+    private final ConfigureAGVController controller = new ConfigureAGVController();
+    MainMenu mainMenu;
+
+    public ConfigureAGVUI (){
+
+    }
+
+    protected boolean doShow() {
+        try{
+            System.out.println("AGV: \n");
+            final String identifier = Console.readLine("AGV Identifier: ");
+            final int agvDock = Console.readInteger("Base Location(Dock): ");
+            final int autonomy = Console.readInteger("Autonomy: ");
+            final String description = Console.readLine("Description: ");
+            final String model = Console.readLine("Model: ");
+            final Double maxWeightCarry = Console.readDouble("Maximum Weight: ");
+            final String task = Console.readLine("Task: ");
+                controller.configureAGV(Identifier.valueOf(identifier), AGV.valueOfAutonomy(autonomy), AGVDock.valueOf(agvDock), AGV.valueOfDescription(description), Model.valueOf(model), MaxWeightCarry.valueOf(maxWeightCarry), Task.valueOf(task));
+
+            System.out.println("Successful operation!");
+        } catch (final IntegrityViolationException | IllegalAccessException ex){
+            System.out.println("There is already a AGV with that information.");
+        }
+        return false;
+    }
+
+    public String headline() {
+        return "Configure AGV";
+    }
+}
---
+++ base.core/src/main/java/eapli/base/warehousemanagement/application/ConfigureAGVController.java
@@ -0,0 +1,25 @@
+package eapli.base.warehousemanagement.application;
+
+import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.usermanagement.domain.BaseRoles;
+import eapli.base.warehousemanagement.domain.*;
+import eapli.framework.domain.repositories.TransactionalContext;
+import eapli.framework.infrastructure.authz.application.AuthorizationService;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+
+public class ConfigureAGVController {
+
+    private final AGVService agvService = new AGVService();
+    private final AuthorizationService authorizationService = AuthzRegistry.authorizationService();
+    private final TransactionalContext txCtx = PersistenceContext.repositories().newTransactionalContext();
+
+    public AGV configureAGV(Identifier identifier, int autonomy, AGVDock agvDock, String AGVDescription, Model model, MaxWeightCarry maxWeightCarry, Task task) throws IllegalAccessException{
+        authorizationService.ensureAuthenticatedUserHasAnyOf(BaseRoles.POWER_USER, BaseRoles.WAREHOUSE_EMPLOYEE);
+        //begin transaction
+        txCtx.beginTransaction();
+        final AGV agv = agvService.createAGV(identifier,autonomy,agvDock,AGVDescription,model, maxWeightCarry,task);
+        txCtx.commit();
+
+        return agv;
+    }
+}
