1fc89e7722847cde240f4429006daf9cca6f6f4b
João Tiago Araújo <1200584@isep.ipp.pt>
2022-04-30 22:38:06 +0100

    <L2L20-1> US1001: Implementation: updated builder, service, controller and UI

--- base.core/src/main/java/eapli/base/ordersmanagement/product/application/ProductService.java
+++ base.core/src/main/java/eapli/base/ordersmanagement/product/application/ProductService.java
@@ -2,11 +2,8 @@ package eapli.base.ordersmanagement.product.application;
 
 
 import eapli.base.infrastructure.persistence.PersistenceContext;
-import eapli.base.ordersmanagement.category.domain.Category;
-import eapli.base.ordersmanagement.category.domain.CategoryCode;
 import eapli.base.ordersmanagement.product.domain.*;
 import eapli.base.ordersmanagement.product.repositories.ProductRepository;
-import eapli.base.warehousemanagement.domain.*;
 import eapli.framework.application.ApplicationService;
 
 
@@ -15,17 +12,29 @@ public class ProductService {
 
     private final ProductRepository productRepository = PersistenceContext.repositories().products();
 
-    public Product createProduct(UniqueInternalCode uniqueInternalCode, ShortDescription shortDescription, ExtendedDescription extendedDescription, TechnicalDescription technicalDescription, Brand brand, Reference reference, ProductionCode productionCode, Weight weight , Barcode barcode, ProductPriceDetail priceDetail, CategoryCode categoryCode, Dimension dimension, WarehouseID warehouseID, Aisle aisle, Row row, Shelf shelf) {
+/*    public Product createProduct(UniqueInternalCode uniqueInternalCode, ShortDescription shortDescription, ExtendedDescription extendedDescription, TechnicalDescription technicalDescription, Brand brand, Reference reference, ProductionCode productionCode, Weight weight , Barcode barcode, ProductPriceDetail priceDetail, Category categoryCode, Dimension dimension, Aisle aisle, Row row, Shelf shelf) {
 
         final ProductBuilder productBuilder = new ProductBuilder();
         Product product;
         product = productBuilder.build();
-        productBuilder.withUniqueInternalCode(uniqueInternalCode).withShortDescription(shortDescription).withExtendedDescription(extendedDescription).withTechnicalDescription(technicalDescription).withBrand(brand).withReference(reference).withProductionCode(productionCode).withWeight(weight).withBarcode(barcode).withPriceDetail(priceDetail).withCategory(categoryCode).withDimension(dimension).withWarehouseID(warehouseID).withAisle(aisle).withRow(row).withShelf(shelf) ;
+        productBuilder.withUniqueInternalCode(uniqueInternalCode).withShortDescription(shortDescription).withExtendedDescription(extendedDescription).withTechnicalDescription(technicalDescription).withBrand(brand).withReference(reference).withProductionCode(productionCode).withWeight(weight).withBarcode(barcode).withPriceDetail(priceDetail).withCategory(categoryCode).withDimension(dimension).withAisle(aisle).withRow(row).withShelf(shelf) ;
+        this.productRepository.save(product);
+
+        return product;
+    }*/
+
+        public Product createProduct(UniqueInternalCode uniqueInternalCode, ShortDescription shortDescription, ExtendedDescription extendedDescription, TechnicalDescription technicalDescription, Brand brand, Reference reference, ProductionCode productionCode, Weight weight , Barcode barcode, ProductPriceDetail priceDetail, Dimension dimension) {
+
+        final ProductBuilder productBuilder = new ProductBuilder();
+        Product product;
+        product = productBuilder.build();
+        productBuilder.withUniqueInternalCode(uniqueInternalCode).withShortDescription(shortDescription).withExtendedDescription(extendedDescription).withTechnicalDescription(technicalDescription).withBrand(brand).withReference(reference).withProductionCode(productionCode).withWeight(weight).withBarcode(barcode).withPriceDetail(priceDetail).withDimension(dimension);
         this.productRepository.save(product);
 
         return product;
     }
-    public Product createProductMandatory(UniqueInternalCode uniqueInternalCode, ShortDescription shortDescription, ExtendedDescription extendedDescription) {
+
+        public Product createProductMandatory(UniqueInternalCode uniqueInternalCode, ShortDescription shortDescription, ExtendedDescription extendedDescription) {
 
         final ProductBuilder productBuilder = new ProductBuilder();
         Product product;
