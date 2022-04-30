265bed112a300fac2cf74e774818c93360ad5a58
João Tiago Araújo <1200584@isep.ipp.pt>
2022-04-30 20:24:24 +0100

    <L2L20-5> fixed ui and json file

--- base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/UI/UploadFileUI.java
+++ base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/UI/UploadFileUI.java
@@ -1,6 +1,7 @@
 package eapli.base.app.backoffice.console.presentation.UI;
 
 import eapli.base.warehousemanagement.application.UploadFileController;
+import eapli.framework.io.util.Console;
 import eapli.framework.presentation.console.AbstractUI;
 
 import java.util.Scanner;
@@ -14,27 +15,21 @@ public class UploadFileUI extends AbstractUI {
     @Override
     protected boolean doShow() {
         int option = -1;
-        do {
-            System.out.println("==================================================");
-            System.out.println("=======Welcome to Upload File Menu!=======");
-            System.out.println("==================================================");
-            System.out.println("1- Upload JSON file");
-            System.out.println("==================================================");
-            System.out.println("0- Back.");
-            switch (option) {
-                case 0:
-                    System.out.println("Exiting ...");
-                    break;
-                case 1:
-                    uploadFileController.uploadFile("WarehousePlan.json");
-                default:
-                    System.out.println("Option does not exist!");
-                    break;
-            }
-
-        } while (option != 0) ;
             try {
-                System.out.println("Successful operation!");
+                System.out.println("==================================================");
+                System.out.println("=======Welcome to Upload File Menu!=======");
+                System.out.println("==================================================");
+                option = Console.readInteger("1- Upload JSON file\n");
+                System.out.println("==================================================");
+                //option = Console.readInteger("0- Back.\n");
+                if(option == 1){
+                    System.out.println("Successful operation!");
+                    uploadFileController.uploadFile("WarehousePlan.json");
+
+                }else if(option == 0){
+                    System.out.println("Exiting ...");
+                } else System.out.println("Option does not exist!");
+
             } catch (Exception e) {
                 System.out.println("Something went wrong");
             }
