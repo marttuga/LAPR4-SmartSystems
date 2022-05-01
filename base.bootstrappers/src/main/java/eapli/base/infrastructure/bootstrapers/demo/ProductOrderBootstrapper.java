8fe68e785003de81ea24d38b965ffe1a44725cc8
João Tiago Araújo <1200584@isep.ipp.pt>
2022-05-01 21:23:37 +0100

    fixed ProductOrderBootstrapper

--- base.bootstrappers/src/main/java/eapli/base/infrastructure/bootstrapers/demo/ProductOrderBootstrapper.java
+++ base.bootstrappers/src/main/java/eapli/base/infrastructure/bootstrapers/demo/ProductOrderBootstrapper.java
@@ -1,9 +1,6 @@
 package eapli.base.infrastructure.bootstrapers.demo;
-
 import eapli.base.infrastructure.persistence.PersistenceContext;
-import eapli.base.ordersmanagement.customer.domain.Customer;
-import eapli.base.ordersmanagement.customer.domain.CustomerId;
-import eapli.base.ordersmanagement.customer.repositories.CustomerRepository;
+import eapli.base.ordersmanagement.customer.domain.*;
 import eapli.base.ordersmanagement.order.application.NewProductOrderController;
 import eapli.base.ordersmanagement.order.domain.*;
 import eapli.base.ordersmanagement.order.repositories.OrderRepository;
@@ -13,34 +10,30 @@ import eapli.base.ordersmanagement.shoppingCart.domain.ProductItem;
 import eapli.framework.actions.Action;
 import eapli.framework.domain.repositories.ConcurrencyException;
 import eapli.framework.domain.repositories.IntegrityViolationException;
-import eapli.framework.general.domain.model.EmailAddress;
 import eapli.framework.general.domain.model.Money;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.*;
-
 public class ProductOrderBootstrapper implements Action {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ProductOrderBootstrapper.class);
     private final NewProductOrderController newProductOrderController = new NewProductOrderController();
     private final OrderRepository orderRepository = PersistenceContext.repositories().orders();
-    private final CustomerRepository customerRepository = PersistenceContext.repositories().customers();
     private final ProductRepository productRepository = PersistenceContext.repositories().products();
-
     @Override
     public boolean execute() {
-        String customerId = customerRepository.findByCustomerId(customerRepository.valueOf("91YwzQjS")).toString();
-        createProductOrder(customerId, "555", customerId, new Date(23/04/2001), "555", 2, "1", 10);
+
+        createProductOrder("123", "555", new Date(23/04/2001), "555", 2, "1", 10);
         return true;
     }
 
-    private void createProductOrder(String actorId, String orderID, String customerId, Date date,
+    private void createProductOrder(String actorId, String orderID, Date date,
                                     String productId, int amount, String currencyCode, int priceMoney) {
 
         ProductOrder productOrder;
         try {
-            Customer customer = customerRepository.findByCustomerId(new CustomerId(customerId));
+            Customer customer = new Customer(new CustomerId("222"), new CustomerFirstName("Customer1") , new CustomerLastName("CustomerL1") ,
+                    new CustomerEmailAdress("customer1@gmail.com") , new CustomerPhoneNumber("999999999") , new CustomerVATIdentifier("1234567") );
             Calendar calendar = Calendar.getInstance();
             calendar.setTime(date);
             Product product = productRepository.findByProductCode(UniqueInternalCode.valueOf(productId));
@@ -50,7 +43,6 @@ public class ProductOrderBootstrapper implements Action {
             ProductItem productItem = new ProductItem(money,products, amount);
             Set<ProductItem> productItems = new HashSet<ProductItem>();
             productItems.add(productItem);
-
             productOrder = newProductOrderController.registerNewOrder(new OrderActor(actorId, OrderActor.Role.Costumer),
                     new OrderID(orderID), customer, calendar, new LineOrder(productItems, money), new PriceOrder(money, new ShippingCost(money)),
                     PaymentMethod.MBWAY, ShippingMethod.Standart, Status.REGISTERED);
