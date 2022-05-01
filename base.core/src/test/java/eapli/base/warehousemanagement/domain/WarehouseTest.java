8e444708cd7c386b22b9c8668fec6efc194fb2bc
Maria Flores <1201615@isep.ipp.pt>
2022-05-01 17:34:48 +0100

    <L2L20-5> US2001 Implementation: class warehouse test

---
+++ base.core/src/test/java/eapli/base/warehousemanagement/domain/WarehouseTest.java
@@ -0,0 +1,54 @@
+package eapli.base.warehousemanagement.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class WarehouseTest {
+
+    WarehouseID warehouseID = new WarehouseID("2");
+    Plan plan = new Plan(20000, 15000, 100, "cm");
+    Aisle aisle = new Aisle(1,5,1,16,1,16,1,"w+");
+    Row row = new Row(1,5,1,7,1);
+    Shelf shelf = new Shelf(1, 4);
+    AGVDock agvDock = new AGVDock("D1", 1, 3, 1, 3, 1, 3, "l+");
+
+
+    @Test
+    void getWarehouseID() {
+        WarehouseID w = new WarehouseID("1");
+        Warehouse warehouse = new Warehouse(w, plan, aisle, row, shelf, agvDock);
+        Assertions.assertEquals(w, warehouse.getWarehouseID());
+
+    }
+
+    @Test
+    void getPlan() {
+       Warehouse warehouse1 = new Warehouse(warehouseID, plan, aisle, row, shelf, agvDock);
+        Assertions.assertEquals(plan, warehouse1.getPlan());
+    }
+
+    @Test
+    void getAisle() {
+        Warehouse warehouse2 = new Warehouse(warehouseID, plan, aisle, row, shelf, agvDock);
+        Assertions.assertEquals(aisle, warehouse2.getAisle());
+    }
+
+    @Test
+    void getRow() {
+        Warehouse warehouse3 = new Warehouse(warehouseID, plan, aisle, row, shelf, agvDock);
+        Assertions.assertEquals(row, warehouse3.getRow());
+    }
+
+    @Test
+    void getShelf() {
+        Warehouse warehouse4 = new Warehouse(warehouseID, plan, aisle, row, shelf, agvDock);
+        Assertions.assertEquals(shelf, warehouse4.getShelf());
+    }
+
+    @Test
+    void getAGVDock() {
+        Warehouse warehouse5 = new Warehouse(warehouseID, plan, aisle, row, shelf, agvDock);
+        Assertions.assertEquals(agvDock, warehouse5.getAgvDock());
+    }
+}
