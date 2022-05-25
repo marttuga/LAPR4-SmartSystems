9b0dd4a2df7c9c5cc8cc6d4f8cb684a3281abcc2
brunommgoncalves <1190456@isep.ipp.pt>
2022-05-25 21:37:21 +0100

    Agv with order created on bootstrap

--- base.bootstrappers/src/main/java/eapli/base/infrastructure/bootstrapers/demo/AgvBootstrapper.java
+++ base.bootstrappers/src/main/java/eapli/base/infrastructure/bootstrapers/demo/AgvBootstrapper.java
@@ -1,6 +1,8 @@
 package eapli.base.infrastructure.bootstrapers.demo;
 
 import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.ordersmanagement.order.domain.ProductOrder;
+import eapli.base.ordersmanagement.order.repositories.OrderRepository;
 import eapli.base.warehousemanagement.application.ConfigureAGVController;
 import eapli.base.warehousemanagement.domain.*;
 import eapli.base.warehousemanagement.repositories.AGVRepository;
@@ -13,6 +15,7 @@ public class AgvBootstrapper implements Action {
     private static final Logger LOGGER = LoggerFactory.getLogger(AgvBootstrapper.class);
     private final ConfigureAGVController configureAGVController = new ConfigureAGVController();
     private final AGVRepository agvRepository = PersistenceContext.repositories().agv();
+    private final OrderRepository orderRepository = PersistenceContext.repositories().orders();
 
     @Override
     public boolean execute() {
@@ -22,14 +25,17 @@ public class AgvBootstrapper implements Action {
         assert agv1 != null;
         agv1.changeStatus(Status.FREE);
         agvRepository.save(agv1);
+
         AGV agv2 = createAgv("557", 10, agvDock, "test", "test", 10);
         assert agv2 != null;
         agv2.changeStatus(Status.FREE);
         agvRepository.save(agv2);
-        AGV agv3 = createAgv("558", 10, agvDock, "test", "test", 10);
+
+        AGV agv3 = createAgvWithOrder("558", 10, agvDock, "test", "test", 10, "555");
         assert agv3 != null;
         agv3.changeStatus(Status.FREE);
         agvRepository.save(agv3);
+
         return true;
     }
 
@@ -42,6 +48,27 @@ public class AgvBootstrapper implements Action {
         try {
             AGV agv = configureAGVController.configureAGV(new Identifier(identifier), autonomy, agvDock, AGVDescription,
                     new Model(model), new MaxWeightCarry(maxWeightCarry), Status.FREE);
+            return agv;
+        } catch (final ConcurrencyException | IllegalAccessException e) {
+            // ignoring exception. assuming it is just a primary key violation
+            // due to the tentative of inserting a duplicated user
+            LOGGER.warn("Assuming {} already exists (activate trace log for details)", identifier);
+            LOGGER.trace("Assuming existing record", e);
+        }
+        return null;
+    }
+
+    private AGV createAgvWithOrder(String identifier,
+                                    int autonomy,
+                                    AGVDock agvDock,
+                                    String AGVDescription,
+                                    String model,
+                                    int maxWeightCarry,
+                                    String orderId) {
+        try {
+            ProductOrder order = orderRepository.findByOrderID(orderId);
+            AGV agv = configureAGVController.configureAGVWithOrder(new Identifier(identifier), autonomy, agvDock, AGVDescription,
+                    new Model(model), new MaxWeightCarry(maxWeightCarry), order);
             return agv;
         } catch (final ConcurrencyException | IllegalAccessException e) {
             // ignoring exception. assuming it is just a primary key violation
