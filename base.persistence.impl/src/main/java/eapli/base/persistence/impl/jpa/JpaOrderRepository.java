730857474844b4c6ec6b9b81f7e55542865fbb26
Rui Dias <1200963@isep.ipp.pt>
2022-05-26 10:14:13 +0100

    <L2L20-41> US2004 Sql query change

--- base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaOrderRepository.java
+++ base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaOrderRepository.java
@@ -49,8 +49,9 @@ public class JpaOrderRepository extends JpaAutoTxRepository<ProductOrder, String
 
     @Override
     public ProductOrder findOrderByAGV(String ident) {
-        Query q = entityManager().createQuery("SELECT po FROM ProductOrder po " +
-                " WHERE po.orderID.orderIden IN(SELECT a.order.id FROM AGV a  WHERE a.identifier.identifier = :ident)");
+        Query q = entityManager().createQuery(
+                "SELECT order FROM AGV a "+
+                " WHERE a.identifier.identifier = :ident");
             q.setParameter("ident", ident);
         return (ProductOrder) q.getSingleResult();
 
