bd247e3c1c94516f502d2daaea4d41ef1c323196
João Tiago Araújo <1200584@isep.ipp.pt>
2022-05-29 18:48:38 +0100

    <L2L20-42> US2005 - Implementation: added run-dashboard.bat and run-dashboard.sh

---
+++ run-dashboard.bat
@@ -0,0 +1,5 @@
+
+SET BASE_CP=base.dashboard\target\base.dashboard-1.3.0-SNAPSHOT.jar;base.dashboard\target\dependency\*;
+
+REM call the java VM, e.g,
+java -cp %BASE_CP% base.dashboard/src/main/java/web/WebDashboard.java
