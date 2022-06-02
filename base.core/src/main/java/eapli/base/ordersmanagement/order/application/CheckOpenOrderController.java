3de1186fff861c6a9620e8bc0dfe1c0b067ee899
Marta Portugal <1201845@isep.ipp.pt>
2022-06-02 10:34:10 +0100

    controller to check open orders for us1502

---
+++ base.core/src/main/java/eapli/base/ordersmanagement/order/application/CheckOpenOrderController.java
@@ -0,0 +1,26 @@
+package eapli.base.ordersmanagement.order.application;
+
+import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.ordersmanagement.customer.domain.Customer;
+import eapli.base.ordersmanagement.customer.repositories.CustomerRepository;
+import eapli.base.ordersmanagement.order.domain.ProductOrder;
+import eapli.base.ordersmanagement.order.repositories.OrderRepository;
+
+import java.util.List;
+
+public class CheckOpenOrderController {
+    private final OrderService orderService = new OrderService();
+    private final OrderRepository orderRepository = PersistenceContext.repositories().orders();
+    private final CustomerRepository customerRepository = PersistenceContext.repositories().customers();
+
+    public void printOrdersList(List<ProductOrder> orderList) {
+        orderService.printOrdersList(orderList);
+    }
+
+    public List<ProductOrder> findOpenOrders(eapli.base.ordersmanagement.order.domain.Status status,Customer c) {
+        return orderRepository.findOpenOrders(status,c);
+    }
+    public Customer findByCustomerEmail(String c){
+        return customerRepository.findByCustomerEmail(c);
+    }
+}
