fbc949cc35cc1456bad5611c86f5f85bbc5063b1
Marta Portugal <1201845@isep.ipp.pt>
2022-06-06 00:17:14 +0100

    controller added method for us1502

--- base.core/src/main/java/eapli/base/ordersmanagement/CustomerCliOrderServer/application/ProtocolProxyController.java
+++ base.core/src/main/java/eapli/base/ordersmanagement/CustomerCliOrderServer/application/ProtocolProxyController.java
@@ -1,8 +1,7 @@
 package eapli.base.ordersmanagement.CustomerCliOrderServer.application;
 
 import eapli.base.ordersmanagement.CustomerCliOrderServer.BookingToken;
-import eapli.base.ordersmanagement.CustomerCliOrderServer.CsvAddProductProtocolProxy;
-import eapli.base.ordersmanagement.CustomerCliOrderServer.CsvCheckOpenOrdersProtocolProxy;
+import eapli.base.ordersmanagement.CustomerCliOrderServer.CsvProtocolProxy;
 import eapli.base.ordersmanagement.CustomerCliOrderServer.FailedRequestException;
 import eapli.base.ordersmanagement.order.domain.ProductOrder;
 import eapli.base.ordersmanagement.shoppingCart.domain.ShoppingCart;
@@ -11,13 +10,13 @@ import java.io.IOException;
 import java.util.List;
 
 public class ProtocolProxyController {
-    private static final CsvAddProductProtocolProxy proxy = new CsvAddProductProtocolProxy();
-    private static final CsvCheckOpenOrdersProtocolProxy proxyy = new CsvCheckOpenOrdersProtocolProxy();
+    private static final CsvProtocolProxy proxy = new CsvProtocolProxy();
 
-    public BookingToken addProd(final ShoppingCart pi) throws FailedRequestException, IOException {
+
+    public BookingToken addProd(final String pi) throws FailedRequestException, IOException {
         return  proxy.addProd(pi);
     }
-    public BookingToken checkOpenOrders(List<ProductOrder> pi) throws FailedRequestException, IOException {
-        return  proxyy.checkOpenOrders(pi);
+    public BookingToken checkOpenOrders(List<String> pi) throws FailedRequestException, IOException {
+        return  proxy.checkOpenOrders(pi);
     }
 }
