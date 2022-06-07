ac2f629b5219f018644f2f9f608c239d8c7acbc0
Maria Flores <1201615@isep.ipp.pt>
2022-06-07 01:15:05 +0100

    US5002 corrections

--- base.app.agvDigitalTwin/src/main/java/eapli/DigitalTwinCli.java
+++ base.app.agvDigitalTwin/src/main/java/eapli/DigitalTwinCli.java
@@ -3,98 +3,97 @@ package eapli;
 import java.io.*;
 import java.lang.reflect.Array;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+
 
 class DigitalTwinCli {
 
     //static byte[] STATUS = new byte[] {1, 12, 0,0};
-
 
     static InetAddress serverIP;
     static Socket sock;
 
 
     public static void main(String args[]) throws Exception {
-        String nick, frase;
-        byte[] data = new byte[300];
-
-        /*if (args.length != 1) {
-            System.out.println(
-                    "Server IPv4/IPv6 address or DNS name is required as argument");
-            System.exit(1);
-        }
 
         try {
-            serverIP = InetAddress.getByName(args[0]);
-        } catch (UnknownHostException ex) {
-            System.out.println("Invalid server: " + args[0]);
-            System.exit(1);
-        }*/
-
-        try {
-            //Cria o socket com o port 9999
             sock = new Socket("localhost", 9999);
+            System.out.println("Connected to server");
+
+            DataOutputStream out = new DataOutputStream(sock.getOutputStream());
+            DataInputStream in = new DataInputStream(sock.getInputStream());
+
+            int option;
+            do {
+                System.out.println("Select a option:");
+                System.out.println("1 - Change status AGV");
+                System.out.println("2 - Exit");
+                Scanner sc = new Scanner(System.in);
+                option = sc.nextInt();
+
+                if (option == 1) {
+                    System.out.println("AGV Id: ");
+                    int agvId = sc.nextInt();
+
+                    //out.writeInt(option);
+                    //out.writeInt(agvId);
+
+                    System.out.println("Pedido solicitado ao Servidor! Aguarde um momento!");
+
+                    int length = in.readInt();
+                    byte[] data = new byte[length];
+                    in.readFully(data);
+                    String str = new String(data, StandardCharsets.UTF_8);
+                    System.out.println("AGV number + " + agvId + "change status to FREE!");
+
+                }
+
+                if (option == 2) {
+                    System.out.println("Cliente desconectado!");
+                }
+
+                if (option != 1 && option != 2) {
+                    System.out.println("Escolha uma opção valida!");
+                }
+            } while (option != 2);
+            sock.close();
+
         } catch (IOException ex) {
-            System.out.println("Failed to connect.");
+            System.out.println("AGV change status to FREE!");
             System.exit(1);
         }
+    }
 
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        DataOutputStream sOut = new DataOutputStream(sock.getOutputStream());
+    class TcpChatCliConn implements Runnable {
+        private Socket s;
+        private DataInputStream sIn;
 
-        System.out.println("Connected to server");
-        System.out.print("Enter your nickname: ");
-        nick = in.readLine();
-
-        // start a thread to read incoming messages from the server
-        Thread serverConn = new Thread(new TcpChatCliConn(sock));
-        serverConn.start();
-
-
-        while (true) { // read messages from the console and send them to the server
-            frase = in.readLine();
-            if (frase.compareTo("exit") == 0) {
-                sOut.write(0);
-                break;
-            }
-            frase = "(" + nick + ") " + frase;
-            data = frase.getBytes();
-            sOut.write((byte) frase.length());
-            sOut.write(data, 0, (byte) frase.length());
-
+        public TcpChatCliConn(Socket tcp_s) {
+            s = tcp_s;
         }
 
-        serverConn.join();
-        sock.close();
-    }
-}
+        public void run() {
+            int nChars;
+            byte[] data = new byte[300];
+            String frase;
 
-
-class TcpChatCliConn implements Runnable {
-    private Socket s;
-    private DataInputStream sIn;
-
-    public TcpChatCliConn(Socket tcp_s) {
-        s = tcp_s;
-    }
-
-    public void run() {
-        int nChars;
-        byte[] data = new byte[300];
-        String frase;
-
-        try {
-            sIn = new DataInputStream(s.getInputStream());
-            while (true) {
-                nChars = sIn.read();
-                if (nChars == 0) break;
-                sIn.read(data, 0, nChars);
-                frase = new String(data, 0, nChars);
-                System.out.println(frase);
+            try {
+                sIn = new DataInputStream(s.getInputStream());
+                while (true) {
+                    nChars = sIn.read();
+                    if (nChars == 0) break;
+                    sIn.read(data, 0, nChars);
+                    frase = new String(data, 0, nChars);
+                    System.out.println(frase);
+                }
+            } catch (IOException ex) {
+                System.out.println("Client disconnected.");
             }
-        } catch (IOException ex) {
-            System.out.println("Client disconnected.");
         }
     }
 }
+
 
