bb90f10505cf4244f55ae398ea774c98f9c0de32
Maria Flores <1201615@isep.ipp.pt>
2022-06-09 13:58:40 +0100

    Rollback LineOrderRepository

--- base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaLineOrderRepository.java
+++ base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaLineOrderRepository.java
@@ -6,7 +6,7 @@ import eapli.base.ordersmanagement.customer.domain.CustomerId;
 import eapli.base.ordersmanagement.customer.repositories.CustomerRepository;
 import eapli.base.ordersmanagement.order.domain.LineOrder;
 import eapli.base.ordersmanagement.order.domain.LineOrderID;
-//import eapli.base.ordersmanagement.order.repositories.LineOrderRepository;
+import eapli.base.ordersmanagement.order.repositories.LineOrderRepository;
 import eapli.framework.domain.repositories.TransactionalContext;
 import eapli.framework.infrastructure.repositories.impl.jpa.JpaAutoTxRepository;
 
