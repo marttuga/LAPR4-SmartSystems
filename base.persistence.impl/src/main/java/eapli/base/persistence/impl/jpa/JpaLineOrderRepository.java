4c6367413bed3831356e8bcb1faae14c1290ea37
Maria Flores <1201615@isep.ipp.pt>
2022-06-09 16:41:55 +0100

    Corrections

--- base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaLineOrderRepository.java
+++ base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaLineOrderRepository.java
@@ -7,10 +7,11 @@ import eapli.base.ordersmanagement.customer.repositories.CustomerRepository;
 import eapli.base.ordersmanagement.order.domain.LineOrder;
 import eapli.base.ordersmanagement.order.domain.LineOrderID;
 import eapli.base.ordersmanagement.order.repositories.LineOrderRepository;
+import eapli.base.ordersmanagement.shoppingCart.repositories.ProductItemRepository;
 import eapli.framework.domain.repositories.TransactionalContext;
 import eapli.framework.infrastructure.repositories.impl.jpa.JpaAutoTxRepository;
 
-public class JpaLineOrderRepository extends JpaAutoTxRepository<LineOrder, LineOrderID, LineOrderID> {
+public class JpaLineOrderRepository extends JpaAutoTxRepository<LineOrder, LineOrderID, LineOrderID> implements LineOrderRepository {
 
     public JpaLineOrderRepository(TransactionalContext autoTx) {
         super(autoTx, "lineOrderId");
