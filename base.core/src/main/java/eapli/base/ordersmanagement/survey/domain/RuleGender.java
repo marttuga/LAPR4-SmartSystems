9f871ce35a7cd48576ab4d2bfc639d70b0d38e15
Marta Portugal <1201845@isep.ipp.pt>
2022-06-13 23:56:27 +0100

    classes addition for rules of survey

---
+++ base.core/src/main/java/eapli/base/ordersmanagement/survey/domain/RuleGender.java
@@ -0,0 +1,10 @@
+package eapli.base.ordersmanagement.survey.domain;
+
+import eapli.framework.domain.model.ValueObject;
+
+public enum RuleGender implements ValueObject {
+    MALE,
+    FEMALE,
+    OTHER
+
+}
