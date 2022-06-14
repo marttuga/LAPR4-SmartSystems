2fb0c9b9ccc8e43e5b5ffa91e9601543ac098e80
Rui Dias <1200963@isep.ipp.pt>
2022-06-14 14:52:17 +0100

    <L2L20-81> Survey repository added method (findAll)

--- base.core/src/main/java/eapli/base/ordersmanagement/survey/repositories/SurveyRepository.java
+++ base.core/src/main/java/eapli/base/ordersmanagement/survey/repositories/SurveyRepository.java
@@ -4,5 +4,8 @@ import eapli.base.ordersmanagement.survey.domain.AlphanumericCode;
 import eapli.base.ordersmanagement.survey.domain.Survey;
 import eapli.framework.domain.repositories.DomainRepository;
 
+import java.util.List;
+
 public interface SurveyRepository extends DomainRepository<AlphanumericCode,Survey> {
+    public List<Survey> findAll();
 }
