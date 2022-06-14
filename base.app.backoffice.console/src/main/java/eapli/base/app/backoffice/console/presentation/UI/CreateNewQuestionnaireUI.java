93af324840189d57abf38d42ab6f0028c347faa4
Marta Portugal <1201845@isep.ipp.pt>
2022-06-14 17:23:12 +0100

    correction on survey creation UI

--- base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/UI/CreateNewQuestionnaireUI.java
+++ base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/UI/CreateNewQuestionnaireUI.java
@@ -5,6 +5,7 @@ import eapli.base.ordersmanagement.customer.applicaion.RegisterCustomerControlle
 import eapli.base.ordersmanagement.customer.domain.Customer;
 import eapli.base.ordersmanagement.customer.domain.CustomerGender;
 import eapli.base.ordersmanagement.customer.repositories.CustomerRepository;
+import eapli.base.ordersmanagement.order.domain.SalesRegion;
 import eapli.base.ordersmanagement.survey.application.CreateNewQuestionnaireController;
 import eapli.base.ordersmanagement.survey.domain.*;
 import eapli.base.utilitarianClasses.Utils;
@@ -42,55 +43,166 @@ public class CreateNewQuestionnaireUI extends AbstractUI {
         String alphanumericCode = Console.readLine("Survey Alphanumeric code: ");
         String surveyDescription = Console.readLine("Survey Description: ");
         int surveyPeriod = Console.readInteger("Survey period (days): ");
-        int ruleA = Console.readInteger("Minimum age rule: ");
-
-        String ruleG = Console.readLine("Gender rule:(F/M/O)");
-        switch (ruleG.toUpperCase()) {
-            case "F":
-                ruleG = String.valueOf(CustomerGender.FEMALE);
-                break;
-            case "M":
-                ruleG = String.valueOf(CustomerGender.MALE);
-                break;
-            case "O":
-                ruleG = String.valueOf(CustomerGender.OTHER);
-                break;
-        }
-        SurveyRule s = new SurveyRule((ruleA),(ruleG));
-
-        List<Customer> allCustomers = controller.findAllCustomers();
-        List<Customer> customers = new ArrayList<>();
+        List<Customer> allCustomers;
+        List<Customer> customers;
         int numBytes;
-        try {
+        SurveyRule s;
+        int ruleA;
+        String ruleG;
 
-            for (Customer c : allCustomers) {
-                if (c.getCustomerBirthDay().getAge() > s.getRuleAge() && c.getCustomerGender().toString().equals(s.getRuleGender())) {
-                    customers.add(c);
 
-                }System.out.println(c.getCustomerBirthDay().getAge());
-            }
+        int answer= ctrl.showOptionsRules();
+        switch (answer) {
+            case (1):
+                 ruleA = Console.readInteger("Minimum age rule: ");
+                 s = new SurveyRule((ruleA));
 
-            System.out.println(customers);
-            File file = new File(fileName);
-            String extension = FilenameUtils.getExtension(file.getName()).toLowerCase();
-            if (!extension.equals("txt"))
-                throw new IllegalStateException("The file's extension should be .txt!");
-            byte[] surveyFile = new byte[(int) file.length()];
-            FileInputStream fileInputStream = new FileInputStream(file);
-            numBytes = fileInputStream.read(surveyFile);
-            if (numBytes == 0) System.out.println("Please try again.");
-            else {
-                fileInputStream.close();
+               allCustomers = controller.findAllCustomers();
+                customers = new ArrayList<>();
+
                 try {
-                    ctrl.saveSurvey(ctrl.createSurvey(AlphanumericCode.valueOf(alphanumericCode), SurveyDescription.valueOf(surveyDescription), SurveyPeriod.valueOf(surveyPeriod), surveyFile, s, customers));
 
-                } catch (Exception e) {
-                    System.out.println("Error saving the questionnaire: " + e);
+                    for (Customer c : allCustomers) {
+                        if (c.getCustomerBirthDay().getAge() > s.getRuleAge()) {
+                            customers.add(c);
+
+                        }
+                    }
+
+                    System.out.println(customers);
+                    File file = new File(fileName);
+                    String extension = FilenameUtils.getExtension(file.getName()).toLowerCase();
+                    if (!extension.equals("txt"))
+                        throw new IllegalStateException("The file's extension should be .txt!");
+                    byte[] surveyFile = new byte[(int) file.length()];
+                    FileInputStream fileInputStream = new FileInputStream(file);
+                    numBytes = fileInputStream.read(surveyFile);
+                    if (numBytes == 0) System.out.println("Please try again.");
+                    else {
+                        fileInputStream.close();
+                        try {
+                            ctrl.saveSurvey(ctrl.createSurvey(AlphanumericCode.valueOf(alphanumericCode), SurveyDescription.valueOf(surveyDescription), SurveyPeriod.valueOf(surveyPeriod), surveyFile, s, customers));
+
+                        } catch (Exception e) {
+                            System.out.println("Error saving the questionnaire: " + e);
+                        }
+                    }
+                } catch (IOException ex) {
+                    throw new IllegalStateException("Error saving the questionnaire: " + ex);
                 }
-            }
-        } catch (IOException ex) {
-            throw new IllegalStateException("Error saving the questionnaire: " + ex);
+                break;
+
+
+
+
+            case (2):
+                 ruleG = Console.readLine("Gender rule:(F/M/O)");
+                switch (ruleG.toUpperCase()) {
+                    case "F":
+                        ruleG = String.valueOf(CustomerGender.FEMALE);
+                        break;
+                    case "M":
+                        ruleG = String.valueOf(CustomerGender.MALE);
+                        break;
+                    case "O":
+                        ruleG = String.valueOf(CustomerGender.OTHER);
+                        break;
+                }
+                 s = new SurveyRule((ruleG));
+
+                 allCustomers = controller.findAllCustomers();
+                 customers = new ArrayList<>();
+
+                try {
+
+                    for (Customer c : allCustomers) {
+                        if (c.getCustomerGender().toString().equals(s.getRuleGender())) {
+                            customers.add(c);
+
+                        }
+                    }
+
+                    System.out.println(customers);
+                    File file = new File(fileName);
+                    String extension = FilenameUtils.getExtension(file.getName()).toLowerCase();
+                    if (!extension.equals("txt"))
+                        throw new IllegalStateException("The file's extension should be .txt!");
+                    byte[] surveyFile = new byte[(int) file.length()];
+                    FileInputStream fileInputStream = new FileInputStream(file);
+                    numBytes = fileInputStream.read(surveyFile);
+                    if (numBytes == 0) System.out.println("Please try again.");
+                    else {
+                        fileInputStream.close();
+                        try {
+                            ctrl.saveSurvey(ctrl.createSurvey(AlphanumericCode.valueOf(alphanumericCode), SurveyDescription.valueOf(surveyDescription), SurveyPeriod.valueOf(surveyPeriod), surveyFile, s, customers));
+
+                        } catch (Exception e) {
+                            System.out.println("Error saving the questionnaire: " + e);
+                        }
+                    }
+                } catch (IOException ex) {
+                    throw new IllegalStateException("Error saving the questionnaire: " + ex);
+                }
+                break;
+
+
+
+
+            case (3):
+                 ruleA = Console.readInteger("Minimum age rule: ");
+
+                 ruleG = Console.readLine("Gender rule:(F/M/O)");
+                switch (ruleG.toUpperCase()) {
+                    case "F":
+                        ruleG = String.valueOf(CustomerGender.FEMALE);
+                        break;
+                    case "M":
+                        ruleG = String.valueOf(CustomerGender.MALE);
+                        break;
+                    case "O":
+                        ruleG = String.valueOf(CustomerGender.OTHER);
+                        break;
+                }
+                 s = new SurveyRule(ruleA,(ruleG));
+
+                 allCustomers = controller.findAllCustomers();
+                 customers = new ArrayList<>();
+
+                try {
+
+                    for (Customer c : allCustomers) {
+                        if (c.getCustomerBirthDay().getAge() > s.getRuleAge() && c.getCustomerGender().toString().equals(s.getRuleGender())) {
+                            customers.add(c);
+
+                        }
+                    }
+
+                    System.out.println(customers);
+                    File file = new File(fileName);
+                    String extension = FilenameUtils.getExtension(file.getName()).toLowerCase();
+                    if (!extension.equals("txt"))
+                        throw new IllegalStateException("The file's extension should be .txt!");
+                    byte[] surveyFile = new byte[(int) file.length()];
+                    FileInputStream fileInputStream = new FileInputStream(file);
+                    numBytes = fileInputStream.read(surveyFile);
+                    if (numBytes == 0) System.out.println("Please try again.");
+                    else {
+                        fileInputStream.close();
+                        try {
+                            ctrl.saveSurvey(ctrl.createSurvey(AlphanumericCode.valueOf(alphanumericCode), SurveyDescription.valueOf(surveyDescription), SurveyPeriod.valueOf(surveyPeriod), surveyFile, s, customers));
+
+                        } catch (Exception e) {
+                            System.out.println("Error saving the questionnaire: " + e);
+                        }
+                    }
+                } catch (IOException ex) {
+                    throw new IllegalStateException("Error saving the questionnaire: " + ex);
+                }
+                break;
         }
+
+
+
         //}
         return false;
     }
