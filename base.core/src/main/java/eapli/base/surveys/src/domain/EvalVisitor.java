186cf85baab5b9eac4603ce413d8bf8c4a633acc
Marta Portugal <1201845@isep.ipp.pt>
2022-06-15 12:36:17 +0100

    deletion of some classes from surveyrule

--- base.core/src/main/java/eapli/base/surveys/src/domain/EvalVisitor.java
+++ base.core/src/main/java/eapli/base/surveys/src/domain/EvalVisitor.java
@@ -1,12 +1,46 @@
 package eapli.base.surveys.src.domain;
 
+import eapli.base.utilitarianClasses.Utils;
+import eapli.framework.strings.util.StringPredicates;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.Console;
+import java.util.*;
 
 
 public class EvalVisitor extends FormGrammarBaseVisitor<String>{
 
+    public String visitParameterOblig(FormGrammarParser.ParameterObligContext ctx) {
+String a;
+List<String> answers = new ArrayList<>();
 
+if (this.visitParameterOblig(ctx).equals("Optional.")) {
+    System.out.println("press enter to skip question");
+}
+        System.out.println();
+do {
+   a= Utils.readLine("Answer: ");
+   if (StringPredicates.isNullOrEmpty(a) && this.visitParameterOblig(ctx).equals("Mandatory.")){
+       System.out.println("this question is mandatory");
+       if (this.visitParameterOblig(ctx).equals("Optional.")){
+          answers.add(answers.size()-1,"Not Answered");
+       }
+   }else{
+       answers.add(a);
+   }
+
+} while (StringPredicates.isNullOrEmpty(a) && this.visitParameterOblig(ctx).equals("Mandatory."));
+
+   return ctx.getText();
+    }
+
+
+    public String visitParameterDiffType(FormGrammarParser.ParameterDiffTypeContext ctx) {
+        if (this.visitParameterDiffType(ctx).equalsIgnoreCase("Single-Choice.")){
+
+        }
+
+
+        return ctx.getText();
+    }
 }
