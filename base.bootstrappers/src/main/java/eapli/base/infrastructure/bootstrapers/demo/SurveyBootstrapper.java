694541c10c0b40ede02a17862c2d2660f4b72337
brunommgoncalves <1190456@isep.ipp.pt>
2022-06-15 16:16:26 +0100

    Correção Bootstrap

--- base.bootstrappers/src/main/java/eapli/base/infrastructure/bootstrapers/demo/SurveyBootstrapper.java
+++ base.bootstrappers/src/main/java/eapli/base/infrastructure/bootstrapers/demo/SurveyBootstrapper.java
@@ -1,9 +1,11 @@
 package eapli.base.infrastructure.bootstrapers.demo;
 
+import eapli.base.infrastructure.persistence.PersistenceContext;
 import eapli.base.ordersmanagement.answer.domain.Answer;
 import eapli.base.ordersmanagement.answer.domain.AnswerId;
 import eapli.base.ordersmanagement.customer.applicaion.RegisterCustomerController;
 import eapli.base.ordersmanagement.customer.domain.*;
+import eapli.base.ordersmanagement.customer.repositories.CustomerRepository;
 import eapli.base.ordersmanagement.survey.application.CreateNewQuestionnaireController;
 import eapli.base.ordersmanagement.survey.domain.AlphanumericCode;
 import eapli.base.ordersmanagement.survey.domain.SurveyDescription;
@@ -28,20 +30,26 @@ public class SurveyBootstrapper implements Action {
     private final CreateNewQuestionnaireController controller = new CreateNewQuestionnaireController();
 
     private final RegisterCustomerController customerController = new RegisterCustomerController();
+    private final CustomerRepository customerRepository = PersistenceContext.repositories().customers();
 
     @Override
     public boolean execute() {
-        Customer customer = new Customer(CustomerId.valueOf("87654321") , CustomerFirstName.valueOf("Gabi"), CustomerLastName.valueOf("Hall"), CustomerEmailAdress.valueOf("gabi@gmail.com"), CustomerPhoneNumber.valueOf("910477203"), CustomerVATIdentifier.valueOf("9263748"));
-        List<Customer>customers = new ArrayList<>();
-        List<Answer>answerList = new ArrayList<>();
-        customers.add(customer);
+        //Customer customer = new Customer(CustomerId.valueOf("87654321") , CustomerFirstName.valueOf("Gabi"), CustomerLastName.valueOf("Hall"), CustomerEmailAdress.valueOf("gabi@gmail.com"), CustomerPhoneNumber.valueOf("910477203"), CustomerVATIdentifier.valueOf("9263748"));
+        Customer customer1 = customerRepository.findAllCustomers().get(0);
+        Customer customer2 = customerRepository.findAllCustomers().get(1);
+        List<Customer> customers = new ArrayList<>();
+        List<Answer> answerList = new ArrayList<>();
+        customers.add(customer1);
+        customers.add(customer2);
         Map<String, String> answers  = new HashMap<>();
         answers.put("1","A");
         answers.put("2","C");
         answers.put("3","A");
         answers.put("4","D");
-        Answer answer = new Answer( new AnswerId("123"),customer, answers);
-        answerList.add(answer);
+        Answer answer1 = new Answer( new AnswerId("123"), customer1, answers);
+        Answer answer2 = new Answer( new AnswerId("124"), customer2, answers);
+        answerList.add(answer1);
+        answerList.add(answer2);
         File file = new File("base.core/src/main/java/eapli/base/surveys/questionnaire.txt");
         byte[] surveyFile = new byte[(int) file.length()];
         createSurvey("321", "wtv", 10, surveyFile,16,"FEMALE", customers, answerList);
