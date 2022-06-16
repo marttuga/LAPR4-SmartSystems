2b1b128a698dbac19335f9bedd113c61b2e69297
Rui Dias <1200963@isep.ipp.pt>
2022-06-16 17:50:16 +0100

    <L2L20-81> Answer service amount questions answered

--- base.core/src/main/java/eapli/base/ordersmanagement/answer/application/AnswerService.java
+++ base.core/src/main/java/eapli/base/ordersmanagement/answer/application/AnswerService.java
@@ -3,18 +3,30 @@ package eapli.base.ordersmanagement.answer.application;
 import eapli.base.infrastructure.persistence.PersistenceContext;
 import eapli.base.ordersmanagement.answer.domain.Answer;
 import eapli.base.ordersmanagement.answer.repository.AnswerRepository;
-import eapli.base.ordersmanagement.customer.domain.Customer;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 public class AnswerService {
 
     private final AnswerRepository answerRepository = PersistenceContext.repositories().answer();
 
-    public String universe(String id){
-        List<Answer> customerList = answerRepository.universeAnswers(id);
-        String size = String.valueOf(customerList.size());
-        return size;
+    public int universe(String id){
+        List<Answer> answers = answerRepository.universe(id);
+        return answers.size();
+    }
+
+    public int answers(String id){
+        List<Answer> answers = answerRepository.universe(id);
+        Map<String, String> map = new HashMap<>();
+        for (Answer a: answers
+             ) {
+            map = a.getEvery_Answer();
+        }
+        map.entrySet().removeIf(entry -> entry.getValue().equalsIgnoreCase("NOT ANSWERED"));
+        return map.size();
     }
 
 }
