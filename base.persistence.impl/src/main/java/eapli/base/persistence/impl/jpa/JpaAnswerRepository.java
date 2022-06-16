baf372b5ee33512b8819aec9a0867c3850391122
Rui Dias <1200963@isep.ipp.pt>
2022-06-16 17:51:46 +0100

    <L2L20-81> Answer in memory and jpa  repository changes

--- base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaAnswerRepository.java
+++ base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaAnswerRepository.java
@@ -4,9 +4,6 @@ import eapli.base.Application;
 import eapli.base.ordersmanagement.answer.domain.Answer;
 import eapli.base.ordersmanagement.answer.domain.AnswerId;
 import eapli.base.ordersmanagement.answer.repository.AnswerRepository;
-import eapli.base.ordersmanagement.customer.domain.Customer;
-import eapli.base.ordersmanagement.customer.domain.CustomerId;
-import eapli.base.ordersmanagement.customer.repositories.CustomerRepository;
 import eapli.base.ordersmanagement.survey.domain.Survey;
 import eapli.framework.domain.repositories.TransactionalContext;
 import eapli.framework.infrastructure.repositories.impl.jpa.JpaAutoTxRepository;
@@ -15,6 +12,7 @@ import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class JpaAnswerRepository extends JpaAutoTxRepository<Answer, AnswerId, AnswerId> implements AnswerRepository {
     public JpaAnswerRepository(TransactionalContext autoTx) {
@@ -28,19 +26,13 @@ public class JpaAnswerRepository extends JpaAutoTxRepository<Answer, AnswerId, A
 
     @Override
     public List<Answer> universe(String id){
-        Query q = entityManager().createQuery(" SELECT s.answers FROM Survey s WHERE s.id = :id");
+        Query q = entityManager().createQuery(" SELECT s.answers FROM Survey s WHERE s.id.alphanumericCode = :id");
         q.setParameter("id", id);
-        return (q.getResultList());
+        return q.getResultList();
     }
-    //SELECT a.customer FROM Answer a WHERE a IN
 
-    @Override
-    public List<Answer> universeAnswers(String id){
-    List<Answer> answerList = new ArrayList<>();
-    TypedQuery<Answer> query = super.createQuery("SELECT DISTINCT s.answers FROM Survey s", Answer.class);
-        for (Answer a: query.getResultList()) {
-        answerList.add(a);
-    }
-        return answerList;
+    public Map<String, String> answers(){
+        Query q = entityManager().createQuery("SELECT a.every_Answer FROM Answer a");
+        return (Map<String, String>) q.getResultList();
     }
 }
