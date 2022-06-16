88e5163c1d3ecd171e573d003a9aa5ef18bb356f
João Tiago Araújo <1200584@isep.ipp.pt>
2022-06-16 18:49:23 +0100

    <L2L20-86> US5100- Implementation: Updated Position

--- base.core/src/main/java/eapli/base/warehousemanagement/domain/Position.java
+++ base.core/src/main/java/eapli/base/warehousemanagement/domain/Position.java
@@ -2,6 +2,9 @@ package eapli.base.warehousemanagement.domain;
 
 import eapli.framework.domain.model.ValueObject;
 
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Position implements ValueObject, Comparable<Position>{
 
     private int x;
@@ -10,6 +13,10 @@ public class Position implements ValueObject, Comparable<Position>{
     public Position (int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    protected Position() {
+
     }
 
     @Override
