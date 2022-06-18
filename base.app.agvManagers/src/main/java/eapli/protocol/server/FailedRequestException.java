9a863c9d5c633551ec1bc85901bb4290f9322196
Maria Flores <1201615@isep.ipp.pt>
2022-06-18 16:58:53 +0100

    <L2L20-110> US5004: Implementation: protocol server (new package)

--- base.app.agvManagers/src/main/java/eapli/protocol/server/FailedRequestException.java
+++ base.app.agvManagers/src/main/java/eapli/protocol/server/FailedRequestException.java
@@ -18,7 +18,7 @@
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package eapli.protocol.client.server;
+package eapli.protocol.server;
 
 /**
  * @author Fábio Lourenço 2022.05.25
