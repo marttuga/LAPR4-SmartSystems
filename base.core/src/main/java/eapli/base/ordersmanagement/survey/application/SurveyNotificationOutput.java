6cbe896257ad4fbf2c3a8e18f92527d6b70aa89e
Marta Portugal <1201845@isep.ipp.pt>
2022-06-19 16:31:54 +0100

    survey notification output

--- base.core/src/main/java/eapli/base/ordersmanagement/survey/application/SurveyNotificationOutput.java
+++ base.core/src/main/java/eapli/base/ordersmanagement/survey/application/SurveyNotificationOutput.java
@@ -1,86 +1,52 @@
 package eapli.base.ordersmanagement.survey.application;
 
+
 import eapli.base.ordersmanagement.customer.domain.Customer;
 import eapli.base.ordersmanagement.survey.domain.Survey;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 public class SurveyNotificationOutput {
 
     protected SurveyNotificationOutput() {
     }
 
-    public static boolean outPutNotification(Survey s) throws IOException {
-       /* try {
-            String filePath = "surveys\\";
-            String fileName = "SurveyNotification";
-            String fileType = "txt";
+    public static boolean outPutNotification(List<Customer> customers) throws IOException {
 
-            for (Customer c : s.getCustomers()) {
-                File myFile = new File(filePath+fileName + c.getCustomerFirstName() + "." + fileType);
-                // String outputLocation = null;
-                //File file = new File("base.core\\src\\main\\java\\eapli\\base\\surveys\\SurveyNotification.txt");
-                // outputLocation = myFile.getAbsolutePath();
-                //myWriter = new FileWriter(outputLocation, true);
 
-                try (FileWriter myWriter = new FileWriter(myFile)) {
-                    myWriter.write("To: " + c.getCustomerEmailAddress());
-                    myWriter.write(" \nHello " + c.getCustomerFirstName() + ", ");
-                    myWriter.write(" \nYou are now able to answer this new questionnaire available in your menu: ");
-                    myWriter.write(" \nID: " + s.getAlphanumericCode() + "Description: " + s.getSurveyDescription());
-                    //myWriter.write(not+"\n");
-                }
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            System.out.println("Error outputting the form request to a txt!");
-        }*/
-        /*try {
-            for (Customer fa : s.getCustomers()) {
-                FileWriter myWriter;
-                String outputLocation = "base.core//src//main//java//eapli//base//surveys//notifications" +fa.getCustomerFirstName() + "SurveyNotification.txt";
-                myWriter = new FileWriter(outputLocation);
-
-                try {
-                myWriter.write("To: " + fa.getCustomerEmailAddress());
-                myWriter.write(" \nHello " + fa.getCustomerFirstName() + ", ");
-                myWriter.write(" \nYou are now able to answer this new questionnaire available in your menu: ");
-                myWriter.write(" \nID: " + s.getAlphanumericCode() + "Description: " + s.getSurveyDescription());
-
-                myWriter.write("");
-                } finally {
-                    myWriter.close();
-                }
-
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            System.out.println("Error outputting the form request to a txt!");
-            return false;
-        }
-        return true;*/
         try {
-            FileWriter myWriter;
+      /*      FileWriter myWriter;
             String outputLocation;
             for (Customer fa : s.getCustomers()) {
                 File myFile = new File("surveys//"+"notification" + fa.getCustomerFirstName().toString()+ "_Survey.txt");
                 myWriter = new FileWriter(myFile, true);
+*/
+            FileWriter myWriter;
+            String outputLocation = null;
+            File file = new File("base.core//src//main//java//eapli//base//surveys//SurveyNotification.txt");
+            outputLocation = file.getAbsolutePath();
+            myWriter = new FileWriter(outputLocation, true);
 
-                try {
+            try {
 
-                    myWriter.write("To: " + fa.getCustomerEmailAddress());
-                    myWriter.write(" \nHello " + fa.getCustomerFirstName() + ", ");
-                    myWriter.write(" \nYou are now able to answer this new questionnaire available in your menu: ");
-                    myWriter.write(" \nID: " + s.getAlphanumericCode() + "Description: " + s.getSurveyDescription());
+                    myWriter.write("Clients able to answer: \n"+ customers.toString());
+                    //myWriter.write(" \nHello " + c.getCustomerFirstName() + ", ");
+                    //myWriter.write(" \nYou are now able to answer this new questionnaire available in your menu: ");
+                    //myWriter.write(" \nID: " + s.getAlphanumericCode() + "Description: " + s.getSurveyDescription());
 
 
-                    myWriter.write("");
-                } finally {
-                    myWriter.close();
-                }
+                   // myWriter.write("");
+
+
+
+
+            } finally {
+                myWriter.close();
             }
+
         } catch (Exception ex) {
             ex.printStackTrace();
             System.out.println("Error outputting the form request to a txt!");
