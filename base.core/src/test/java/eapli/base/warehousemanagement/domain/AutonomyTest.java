b8269d7a411a70c43113a4c1c93a828db9d1bf5a
João Tiago Araújo <1200584@isep.ipp.pt>
2022-06-19 12:30:10 +0100

    <L2L20-86> US5100- Test: Added AutonomyTest

---
+++ base.core/src/test/java/eapli/base/warehousemanagement/domain/AutonomyTest.java
@@ -0,0 +1,34 @@
+package eapli.base.warehousemanagement.domain;
+
+import eapli.base.ordersmanagement.customer.domain.CustomerPhoneNumber;
+import eapli.base.ordersmanagement.customer.domain.CustomerVATIdentifier;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutonomyTest {
+
+    @Test
+    void compareTo() throws IllegalAccessException {
+        Autonomy a1 = new Autonomy(100);
+        Autonomy a2 = new Autonomy(100);
+        Assertions.assertEquals(0, a1.compareTo(a2));
+    }
+
+    @Test
+    void testToString() throws IllegalAccessException {
+        Autonomy a = new Autonomy(100);
+        Assertions.assertEquals("Autonomy: 100", a.toString());
+    }
+
+    @Test
+    void ensureAutonomyIsNotEmpty() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Autonomy());
+    }
+
+    @Test
+    void ensureAutonomyIsBetween0And100() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Autonomy(100));
+    }
+}
