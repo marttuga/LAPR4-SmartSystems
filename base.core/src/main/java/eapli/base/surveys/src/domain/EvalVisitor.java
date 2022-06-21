3a5950c2a10b520633af0330d25ff77392ffedc6
Marta Portugal <1201845@isep.ipp.pt>
2022-06-21 15:37:21 +0100

    visitor correction

--- base.core/src/main/java/eapli/base/surveys/src/domain/EvalVisitor.java
+++ base.core/src/main/java/eapli/base/surveys/src/domain/EvalVisitor.java
@@ -6,41 +6,48 @@ import java.util.*;
 
 public class EvalVisitor extends FormGrammarBaseVisitor<String> {
 
-
     public String visitParameterAnswer(FormGrammarParser.ParameterAnswerContext ctx) {
         FormGrammarParser.ParameterQuestionContext questionContext = (FormGrammarParser.ParameterQuestionContext) ctx.getParent();
         String obligatoriness = questionContext.parameterQuestion().parameterOblig().getText();
         String d = questionContext.parameterAnswer().getText();
 
+        List<String> error = new ArrayList<>();
+
         String[] words = {"horrible", "awful", "disappointing", "hate"};
         for (String e : words) {
             if (d.contains(e))
-                System.out.println("Error the answer contains inappropriate words and is not accepted: " + e);
+                error.add("Error the answer contains inappropriate words and is not accepted: " + e);
+            //System.out.println("Error the answer contains inappropriate words and is not accepted: " + e);
         }
 
-       if (!questionContext.parameterQuestion().parameterQuestionType().getText().equalsIgnoreCase("Type: Free-Text.")) {
+        if (!questionContext.parameterQuestion().parameterQuestionType().getText().equalsIgnoreCase("Type: Free-Text.")) {
             if (obligatoriness.equals("Obligatoriness: Mandatory.")) {
                 if ((!(d.matches("[1-4]")))) {
-
-                    System.out.println("Error while choosing the option for mandatory question :" + d + " not valid");
+                    error.add("Error while choosing the option for mandatory question :" + d + " not valid");
+                    // System.out.println("Error while choosing the option for mandatory question :" + d + " not valid");
 
                 }
             }
         }
 
         if (d.matches(RandomStringUtils.randomAlphabetic(10).toLowerCase(Locale.ROOT))) {
-            System.out.println("Error while answering, random phrase: " + d);
+            error.add("Error while answering, random phrase: " + d);
+            //System.out.println("Error while answering, random phrase: " + d);
 
         }
         if ((obligatoriness.equals("Obligatoriness: Mandatory.") && (questionContext.parameterQuestion().parameterQuestionType().getText().equalsIgnoreCase("Type: Free-Text.")))) {
             if (d.length() == 1 || d.length() > 50) {
-                System.out.println("Error while answering a mandatory question, length of answer is not valid: " + d );
+                error.add("Error while answering a mandatory question, length of answer is not valid: " + d);
+                //System.out.println("Error while answering a mandatory question, length of answer is not valid: " + d);
             }
+        }
 
+        for (String fg : error) {
+            System.out.println(fg);
 
         }
+
         return "Answers are all checked";
     }
-
 
 }
